99920d80b61af70095b04e5c224b8593162c38b6
JustSaagar <anghansagar25@gmail.com>
2022-07-10 21:36:25 +0530

    Add : Required validation for email

--- backend_logic/userApi.py
+++ backend_logic/userApi.py
@@ -1,18 +1,30 @@
 import uuid
 from flask import Blueprint, request, jsonify
 from firebase_admin import firestore
+from flask_expects_json import expects_json
+from jsonschema import ValidationError
 
 db = firestore.client()
 user_ref = db.collection("pythonUser")
 
 userApi = Blueprint('userAPi', __name__)
 
+schema = {
+    "type": "object",
+    "properties": {
+        "name": {"type": "string"},
+        "email": {"type": "string"}
+    },
+    "required": ["email"]
+}
+
 
 @userApi.route('/add', methods=['POST'])
+@expects_json(schema)
 def create():
     try:
         id = uuid.uuid4()
         user_ref.document(id.hex).set(request.json)
         return jsonify({"status": "{id} created successfully"}), 200
-    except Exception as e:
-        return f"An error : {e}"
+    except ValidationError as e:
+        return jsonify({"status": e.message}), 400
--- main.py
+++ main.py
@@ -2,6 +2,5 @@ from backend_logic import _init__
 
 app = _init__.createApp()
 
-
 if __name__ == '__main__':
     app.run(debug = True)
