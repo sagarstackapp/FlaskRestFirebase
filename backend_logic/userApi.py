bcedffd7e0d0add4294618fd8744a88a22eda0a1
Dev <sagaranghan.stackapp@gmail.com>
2022-07-11 10:54:32 +0530

    Add : Get user data API

--- backend_logic/userApi.py
+++ backend_logic/userApi.py
@@ -12,11 +12,28 @@ userApi = Blueprint('userAPi', __name__)
 
 
 @userApi.route('/createUser', methods=['POST'])
-@required_validation.required_params({"firstName": str, "lastName": str, "email": str, "phoneNumber": int})
+@required_validation.required_params({"id": str, "firstName": str, "lastName": str, "email": str, "phoneNumber": int})
 def create():
     # try:
-    id = uuid.uuid4()
-    user_ref.document(id.hex).set(request.json)
+    id = request.json['id']
+    user_ref.document(id).set(request.json)
     return jsonify({"status": True, "message": f"{id} created successully", }), 201
     # except ValidationError as e:
     #     return jsonify({"status": e.message}), 400
+
+
+@userApi.route('/', methods=['GET'])
+def read():
+    try:
+        id = request.args.get("id")
+        if id:
+            userData = user_ref.document(id).get()
+            if userData.exists:
+                return jsonify({'status': True, 'data': userData.to_dict(), 'message': 'Record fetched successfully'}), 200
+            else:
+                return jsonify({'status': False, 'data': [], 'message': 'No records found'}), 200
+        else:
+            userData = [doc.to_dict() for doc in user_ref.stream()]
+            return jsonify({'status': True, 'data': userData, 'message': 'Records fetched successfully'}), 200
+    except ValidationError as e:
+        return jsonify({'status': e.message}), 400
